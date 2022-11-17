9499043752489ed86dd7e708c694206e728b395b
rsilva070589 <rsilva070589@gmail.com>
2022-11-17 17:32:49 +0100

    first commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+yarn build
+ 
+git init 
+
+git config --global user.email "rsilva070589@gmail.com"
+git config --global user.name "rsilva070589"
+
+
+git add .
+ 
+
+git commit -m "first commit"
+ 
+ 
+git branch -M main
+git remote add origin https://github.com/rsilva070589/templateExemplo.git
+
+git push -f origin main
+ 
